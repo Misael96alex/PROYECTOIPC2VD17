91479d94d7f8b86d3be40264ba82e3f921eba3ea
Misael Ajuchan <toony-a@hotmail.com>
2017-12-24 15:26:13 -0600

    14 comit: creacion de las clases de coneccion y usuarios, importacion de librerias mariadb

--- Codigo/ProyectoIPC2/src/java/pack/Coneccion.java
+++ Codigo/ProyectoIPC2/src/java/pack/Coneccion.java
@@ -1,6 +1,19 @@
 package pack;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Coneccion {
-    
+    public Connection conexion;
+    public Coneccion() throws ClassNotFoundException, SQLException{
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            conexion = DriverManager.getConnection("jdbc:mariadb://localhost3306/proyecto?user=root?password=misael96");
+            java.sql.Statement s= conexion.createStatement();
+            
+        } catch (ClassNotFoundException | SQLException e) {
+        }
+    }
 }
