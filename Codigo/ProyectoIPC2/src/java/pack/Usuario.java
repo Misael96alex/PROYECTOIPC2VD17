bcca55c9cf0545f375d0c54b92f4269988a30d2a
Misael Ajuchan <toony-a@hotmail.com>
2017-12-24 15:27:23 -0600

    15 comit: modificacion de clases

--- Codigo/ProyectoIPC2/src/java/pack/Usuario.java
+++ Codigo/ProyectoIPC2/src/java/pack/Usuario.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pack;
 
-/**
- *
- * @author Alex
- */
+import java.sql.SQLException;
+
 public class Usuario {
+    
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        
+    }
     
 }
